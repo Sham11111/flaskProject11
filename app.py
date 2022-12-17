5daae68fe6a1fbe61e7dde5eb9ddca338ebe795f
Sham11111 <108691285+Sham11111@users.noreply.github.com>
2022-12-17 23:44:56 +0300

    Главная страница сайта

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, request, render_template, url_for, redirect, session
+from flask import Flask, request, render_template, url_for, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
-
 import atexit
-
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ad.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'urfd1123'
+app.secret_key = 'gjdslgnfwelje124804810'
 db = SQLAlchemy(app)
 
 scheduler = BackgroundScheduler()
@@ -25,102 +23,35 @@ class DBF(db.Model):
     def __repr__(self):
         return 'DBF %r' % self.id
 
-
-def delete_by_time():
-    with app.app_context():
-
-        tables = DBF.query.all()
-
-        for item_tables in tables:
-
-            if item_tables.tm + timedelta(minutes=5) <= datetime.now():
-                db.session.delete(item_tables)
-                db.session.commit()
-
-
-scheduler.add_job(func=delete_by_time, trigger="interval", seconds=360)
-
-scheduler.start()
-
-atexit.register(lambda: scheduler.shutdown())
-
-
+# Осуществление входа на сайт под своим именем
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if not session:
         if request.method == 'POST':
             session['username'] = request.form['username']
             return redirect(url_for('get_ad'))
         else:
-            return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-        '''
-    else:
-        return redirect(url_for('get_ad'))
+            return redirect(url_for('get_ad'))
 
 
 @app.route('/')
 def redirect_all():
     return redirect(url_for('login'))
 
-
+# Вызов объявления
 @app.route('/all', methods=['POST', 'GET'])
-def get_ad():
-    """Вызов объявления"""
+def disp_ads():
     gad = DBF.query.order_by(DBF.tm.desc()).all()
-    return render_template("all.html", gad=gad)
+    return render_template("all_ads.html", gad=gad)
 
-
+# Подробности объявления
 @app.route('/all/<int:id>')
-def detail(id):
-    """Содержание объявления"""
+def detail_ads(id):
     gad_detail = DBF.query.get(id)
-    return render_template("ad_detail.html", gad_detail=gad_detail)
+    return render_template("detail_ads.html", gad_detail=gad_detail)
 
-
-@app.route('/all/<int:id>/delete')
-def del_ad(id):
-    """Удаление объявлений"""
-    gad_detail = DBF.query.get_or_404(id)
-    gad_detail.username = session['username']
-
-    if gad_detail.username == session['username']:
-        return "Ошибка! Вы можете удалить только свое объявление!"
-    else:
-        try:
-            db.session.delete(gad_detail)
-            db.session.commit()
-            return redirect('/all')
-        except:
-            return "Ошибка!"
-
-
-@app.route('/all/<int:id>/update', methods=['POST', 'GET'])
-def update_ad(id):
-    """Изменение объявления"""
-    gad_detail = DBF.query.get(id)
-    if request.method == "POST":
-        gad_detail.username = session['username']
-        gad_detail.title = request.form['title']
-        gad_detail.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect('/all')
-        except:
-            return "Ошибка!"
-    else:
-        if gad_detail.username != session['username']:
-            return "Ошибка! Вы можете изменить только свое объявление!"
-        else:
-            return render_template("update_ad.html", gad_detail=gad_detail)
-
-
+# Создание объявлений
 @app.route('/create_ad', methods=['POST', 'GET'])
-def create_ad():
-    """Создание объявления"""
+def create_ads():
     if request.method == "POST":
         username = session['username']
         title = request.form['title']
@@ -135,8 +66,58 @@ def create_ad():
         except:
             return "Ошибка!"
     else:
-        return render_template("create_ad.html")
+        return render_template("create_ads.html")
+
+# Внесение изменений в объявления
+@app.route('/all/<int:id>/update', methods=['POST', 'GET'])
+def update_ad(id):
+    gad_detail = DBF.query.get(id)
+    if request.method == "POST":
+        gad_detail.username = session['username']
+        gad_detail.title = request.form['title']
+        gad_detail.content = request.form['content']
+
+        db.session.commit()
+        return redirect('/all')
+
+    else:
+        if gad_detail.username != session['username']:
+            return "Ошибка! Вы можете изменить только свои объявления!"
+        else:
+            return render_template("update_ads.html", gad_detail=gad_detail)
+
+# Удаление объявлений
+@app.route('/all/<int:id>/delete')
+def delete_ads(id):
+    gad_detail = DBF.query.get(id)
+    if request.method == 'GET':
+        if gad_detail.username == session['username']:
+            db.session.delete(gad_detail)
+            db.session.commit()
+            return redirect('/all')
+
+        else:
+            return "Ошибка! Вы можете удалять только свои объявления!"
+
+# Автоматическое удаление объявлений по времени
+def auto_delete():
+    with app.app_context():
+
+        tables = DBF.query.all()
+
+        for item_tables in tables:
+
+            if item_tables.tm + timedelta(minutes=5) <= datetime.now():
+                db.session.delete(item_tables)
+                db.session.commit()
+
+
+scheduler.add_job(func=auto_delete, trigger="interval", seconds=360)
+
+scheduler.start()
+
+atexit.register(lambda: scheduler.shutdown())
 
 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run()
